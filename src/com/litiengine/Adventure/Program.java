abe3780a8e74233f3e87a70d3235927dd123ae78
William How <56580596+KoalaaDev@users.noreply.github.com>
2023-07-11 20:57:13 +0400

    Add files via upload

---
+++ src/com/litiengine/Adventure/Program.java
@@ -0,0 +1,11 @@
+package com.litiengine.Adventure;
+
+import de.gurkenlabs.litiengine.Game;
+
+public class Program {
+
+  public static void main(String[] args) {
+    Game.init(args);
+    Game.start();
+  }
+}
